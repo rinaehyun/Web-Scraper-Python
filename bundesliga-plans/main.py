5e8364e674ee4520e359d4cad95b02b5b237fdf6
rinaehyun <rnhyun.dream@gmail.com>
2024-02-28 12:35:44 +0100

    Add headers in requests.get method

--- bundesliga-plans/main.py
+++ bundesliga-plans/main.py
@@ -1,13 +1,22 @@
 import requests
+#import selectorlib
 
 URL = "https://www.bundesliga.com/de/bundesliga/spieltag"
-
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/39.0.2171.95 Safari/537.36',
+    'Content-Type': 'application/json'
+}
 
 def scrape(url):
     """ Scrape the page source from the URL """
-    response = requests.get(url)
+    response = requests.get(url, headers=HEADERS)
     source = response.text
     return source
+
+
+#def extract(source):
+#    extractor = select
 
 
 if __name__ == "__main__":
