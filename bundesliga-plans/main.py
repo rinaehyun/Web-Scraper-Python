89f7e2aa74cdde599d3057337af6096e067738bf
rinaehyun <rnhyun.dream@gmail.com>
2024-02-29 09:23:25 +0100

    Create Classes for scraping and saving the data to txt

--- bundesliga-plans/main.py
+++ bundesliga-plans/main.py
@@ -11,36 +11,35 @@ HEADERS = {
 
 connection = sqlite3.connect("../databases/match-data.db")
 
-def scrape(url):
-    """ Scrape the page source from the URL """
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
+
+class MatchPlans:
+    def scrape(self, url):
+        """ Scrape the page source from the URL """
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
+
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file('match-plans.yaml')
+        match_info = extractor.extract(source)["match-info"]
+        match_teams = extractor.extract(source)["match-teams"]
+        return {"match-info": match_info,
+                "teams": match_teams}
+
+    def process(self, extracted_content):
+        match_number = extracted_content["match-info"].partition("Saison 2023-2024")[0].strip()
+        match_info = tuple([match_number])
+        match_teams = extracted_content["teams"]
+        match_teams_vs = list(zip(match_teams[::2], match_teams[1::2]))
+        values_sql = [match_info + team for team in match_teams_vs]
+        return values_sql
 
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file('match-plans.yaml')
-    match_info = extractor.extract(source)["match-info"]
-    match_teams = extractor.extract(source)["match-teams"]
-    return {"match-info": match_info,
-            "teams": match_teams}
-
-
-def process(extracted_content):
-    match_number = extracted_content["match-info"].partition("Saison 2023-2024")[0].strip()
-    match_info = tuple([match_number])
-
-    match_teams = extracted_content["teams"]
-    match_teams_vs = list(zip(match_teams[::2], match_teams[1::2]))
-    values_sql = [match_info + team for team in match_teams_vs]
-
-    return values_sql
-
-
-def save_as_text(data):
-    text = "\n".join(str(el) for el in data)
-    with open('match.txt', 'w') as file:
-        file.write(text)
+class TextFile:
+    def save(self, data):
+        text = "\n".join(str(el) for el in data)
+        with open('match.txt', 'w') as file:
+            file.write(text)
 
 
 def store_to_sql(data):
@@ -53,18 +52,20 @@ def read_from_sql(data):
     match_number = data["match-info"].partition("Saison 2023-2024")[0].strip()
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM MatchInfos WHERE match_number=?", (match_number,))
-    row_from_sql = cursor.fetchall()
-    return row_from_sql
+    records_from_sql = cursor.fetchall()
+    return records_from_sql
 
 
 if __name__ == "__main__":
-    content = scrape(URL)
-    extracted = extract(content)
+    matches = MatchPlans()         # Create a instance 'matches' by calling the class MatchPlans()
+    content = matches.scrape(URL)
+    extracted = matches.extract(content)
 
-    row = read_from_sql(extracted)
-    if len(row) == 0:
-        processed = process(extracted)
-        save_as_text(processed)
+    records = read_from_sql(extracted)
+    if len(records) == 0:
+        processed = matches.process(extracted)
+        txt_file = TextFile()
+        txt_file.save(processed)
         store_to_sql(processed)
         print("New info about the Bundesliga matches is inserted into the database.")
     else:
